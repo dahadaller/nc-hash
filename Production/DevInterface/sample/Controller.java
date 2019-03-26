7f2e1cfa26f3409960c75d90f0b47883f7be9b3c
bonliu <cheukbong1210@gmail.com>
2019-03-26 13:43:42 -0400

    Added sample interface

---
+++ Production/DevInterface/sample/Controller.java
@@ -0,0 +1,21 @@
+package sample;
+
+import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class Controller implements Initializable {
+
+
+
+    public void loginButtonClicked() {
+        System.out.println("User logged in...");
+    }
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+        System.out.println("Loading use data...");
+    }
+}
