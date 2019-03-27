ea126dd07ba084ccf4b9e7f22f43459e7fd90fbe
Jonathan <jtran001@citymail.cuny.edu>
2019-03-27 00:21:01 -0400

    Bon's code now works. Had to move files and overwrite old files. Also removed NC-Hash.iml since the name of it is now production.

--- Production/src/sample/Controller.java
+++ Production/src/sample/Controller.java
@@ -1,4 +1,21 @@
 package sample;
 
-public class Controller {
+import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class Controller implements Initializable {
+
+
+
+    public void loginButtonClicked() {
+        System.out.println("User logged in...");
+    }
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+        System.out.println("Loading use data...");
+    }
 }
--- Production/src/sample/Main.java
+++ Production/src/sample/Main.java
@@ -8,16 +8,17 @@ import javafx.stage.Stage;
 
 public class Main extends Application {
 
+    public static void main(String[] args) {
+        launch(args);
+    }
+
     @Override
     public void start(Stage primaryStage) throws Exception{
         Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
         primaryStage.setTitle("Hello World");
-        primaryStage.setScene(new Scene(root, 300, 275));
+        primaryStage.setScene(new Scene(root, 800, 500));
         primaryStage.show();
     }
 
 
-    public static void main(String[] args) {
-        launch(args);
-    }
 }
