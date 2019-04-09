c43832970f05aec139b8dab54243510852dcdb7f
Jonathan Tran <43768831+jontran1@users.noreply.github.com>
2019-04-09 13:24:25 -0400

    Refactor's Bon's code
    just changed a few things.

--- Production/src/com/nchash/Main.java
+++ Production/src/com/nchash/Main.java
@@ -1,9 +1,12 @@
 package com.nchash;
 
+import com.nchash.controller.Controller;
 import javafx.application.Application;
+import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.layout.BorderPane;
 import javafx.stage.Stage;
 
 public class Main extends Application {
@@ -12,13 +15,35 @@ public class Main extends Application {
         launch(args);
     }
 
+    private Stage primaryStage;
+
     @Override
     public void start(Stage primaryStage) throws Exception{
-        Parent root = FXMLLoader.load(getClass().getResource("view/sample.fxml"));
-        primaryStage.setTitle("Hello World");
-        primaryStage.setScene(new Scene(root, 800, 500));
-        primaryStage.show();
+        this.primaryStage = primaryStage;
+        showDebugScreen();
     }
+
+    /**
+     * Shows Bon's code which is the debug window
+     */
+    public void showDebugScreen(){
+        try{
+            FXMLLoader loader = new FXMLLoader();
+            loader.setLocation(Main.class.getResource("view/sample.fxml"));
+            BorderPane debugScreenView = (BorderPane)loader.load();
+
+            Scene scene = new Scene(debugScreenView);
+            Controller controller = loader.getController();
+            controller.setDebugStage(primaryStage);
+            primaryStage.setScene(scene);
+            primaryStage.show();
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+
+    }
+
+
 
 
 }
--- Production/src/com/nchash/controller/Controller.java
+++ Production/src/com/nchash/controller/Controller.java
@@ -2,20 +2,26 @@ package com.nchash.controller;
 
 import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
+import javafx.stage.Stage;
 
 import java.net.URL;
 import java.util.ResourceBundle;
 
 public class Controller implements Initializable {
 
-
+    private Stage debugStage;
 
     public void loginButtonClicked() {
         System.out.println("User logged in...");
+        debugStage.close();
     }
 
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
         System.out.println("Loading use data...");
+    }
+
+    public void setDebugStage(Stage debugStage) {
+        this.debugStage = debugStage;
     }
 }
