6b2731ce79563d4aada89ceac85d0a766605bdfe
Jonathan Tran <43768831+jontran1@users.noreply.github.com>
2019-04-16 00:20:27 -0400

    Added default upload image and upload button.

--- Production/src/com/nchash/controller/MainController.java
+++ Production/src/com/nchash/controller/MainController.java
@@ -4,7 +4,10 @@ import javafx.fxml.FXML;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 
+
 public class MainController {
+    private final static String defaultUploadImage = "file:src/com/nchash/images/drag_and_drop_image.gif";
+
     @FXML
     private ImageView mainImageView;
 
@@ -14,7 +17,7 @@ public class MainController {
      */
     @FXML
     public void initialize(){
-      //  mainImageView.setImage(new Image("./images/drag_and_drop_image.gif"));
+       mainImageView.setImage(new Image(defaultUploadImage));
 
     }
 
