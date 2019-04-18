f6a8e935236393811886c7c40426ec5772e2a07c
Jonathan Tran <43768831+jontran1@users.noreply.github.com>
2019-04-18 00:43:42 -0400

    Added in file explorer for upload button and drag and drop feature.

--- Production/src/com/nchash/controller/MainController.java
+++ Production/src/com/nchash/controller/MainController.java
@@ -1,8 +1,15 @@
 package com.nchash.controller;
 
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
+import javafx.scene.input.DragEvent;
+import javafx.scene.input.TransferMode;
+import javafx.stage.FileChooser;
+
+import java.io.File;
+import java.io.FileNotFoundException;
 
 
 public class MainController {
@@ -17,8 +24,91 @@ public class MainController {
      */
     @FXML
     public void initialize(){
-       mainImageView.setImage(new Image(defaultUploadImage));
+       setDefaultUploadImage(defaultUploadImage);
 
+    }
+
+    /**
+     * This function opens the file explorer window. The file selected must be an image
+     */
+    public void handleUploadBtn(ActionEvent event){
+        String pathToFile = openFileExplorer();
+        if(pathToFile != null){
+            setDefaultUploadImage(pathToFile);
+        }
+
+
+    }
+
+
+    /**
+     * This function will open the file explorer. The user will pick a file
+     * and the path of the file will be returned else returns null.
+     * @return path to file String. Null if no file is selected.
+     */
+    private String openFileExplorer(){
+        FileChooser fileChooser = new FileChooser();
+        fileChooser.setTitle("Open File Explorer");
+        File imageFile = fileChooser.showOpenDialog(null);
+        if(imageFile != null){
+            StringBuilder pathToFile = new StringBuilder();
+            pathToFile.append("file:");
+            pathToFile.append(imageFile.getAbsolutePath());
+            return pathToFile.toString();
+        }
+        return null;
+    }
+
+    /**
+     * This function will handle the drag file feature.
+     * If the file is accepted the handle drop function will be called else
+     * the previous image will remain.
+     * @param event
+     */
+    @FXML
+    private void handleDragOver(DragEvent event){
+        if(event.getDragboard().hasFiles()){
+            event.acceptTransferModes(TransferMode.ANY);
+        }
+    }
+
+    /**
+     * Once the file has been accepted after the drag function is called.
+     * This function will be called and returns a image.
+     * @param event
+     * @return A string containing the path to file.
+     * @throws FileNotFoundException
+     */
+    @FXML
+    private void handleDrop(DragEvent event){
+        try {
+            String pathToFile = getPathToImageDragAndDrop(event);
+            System.out.println(pathToFile);
+            setDefaultUploadImage(pathToFile);
+        }catch (Exception e){
+            e.getCause().printStackTrace();
+        }
+    }
+
+    /**
+     * This function takes in an DragEvent object and returns a String
+     * containing the path to the image.
+     * @param event
+     * @return
+     * @throws FileNotFoundException
+     */
+    private String getPathToImageDragAndDrop(DragEvent event)throws FileNotFoundException{
+        StringBuilder pathToImage = new StringBuilder("File:");
+        pathToImage.append(event.getDragboard().getFiles().get(0));
+        return pathToImage.toString();
+    }
+
+    /**
+     * Sets the main image in application to any image.
+     * @param pathToFile
+     */
+    private void setDefaultUploadImage(String pathToFile){
+        mainImageView.setImage(new Image(pathToFile));
     }
 
 }
