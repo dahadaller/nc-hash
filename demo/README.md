7271116e5eac73e69355fba5a7f30298dff177d6
bonliu <cheukbong1210@gmail.com>
2019-04-27 16:08:39 -0400

    Added #7 and #8 manipulation

--- demo/README.md
+++ demo/README.md
@@ -9,14 +9,23 @@
 4. Vertical Flipping
 5. Invert pixel values
 6. JPEG Compression (Mac's default rate)
+7. Rotate by specified degrees
+8. Percentage change of each pixel values
 
-| Manipulations       | Hash (before) | Hash (after) | Distance |
-|:-------------------:|:-------------:|:------------:|:--------:|
-| Cropping            | 21194984      | 16213941     | 0.56     |
-| Left Rotate         | 22267473      | 21941111     | 0.40     |
-| Right Rotate        | 32023788      | 32291200     | 0.36     |
-| Horizontal Flip     | 28224592      | 27330120     | 0.40     |
-| Vertical Flip       | 24303572      | 24704224     | 0.32     |
-| Invert pixel values | 16854459      | 17267339     | 0.36     |
-| JPEG Compress       | 32846954      | 29239466     | 0.36     |
-
+| Manipulations          | Hash (before) | Hash (after) | Distance |
+|:----------------------:|:-------------:|:------------:|:--------:|
+| Cropping               | 21194984      | 16213941     | 0.56     |
+| Left Rotate            | 22267473      | 21941111     | 0.40     |
+| Right Rotate           | 32023788      | 32291200     | 0.36     |
+| Horizontal Flip        | 28224592      | 27330120     | 0.40     |
+| Vertical Flip          | 24303572      | 24704224     | 0.32     |
+| Invert pixel values    | 16854459      | 17267339     | 0.36     |
+| JPEG Compress          | 32846954      | 29239466     | 0.36     |
+| Rotate 05 degree       | 33704654      | 29683255     | 0.58     |
+| Rotate 10 degree       | 34438147      | 34906570     | 0.50     |
+| Rotate 20 degree       | 41281249      | 42932136     | 0.54     |
+| Rotate 30 degree       | 29785108      | 30401221     | 0.44     |
+| Rotate 40 degree       | 30536468      | 29818905     | 0.48     |
+| 25% * pixel_val        | 22860828      | 48778480     | 0.54     |
+| 50% * pixel_val        | 15877022      | 20929632     | 0.72     |
+| 75% * pixel_val        | 82890379      | 84215328     | 0.56     |
--- demo/src/PreProc.py
+++ demo/src/PreProc.py
@@ -1,6 +1,32 @@
 import cv2
+import imutils
 import numpy as np
 from matplotlib import pyplot as plt
+
+def rotate_bound(image, angle):
+    # grab the dimensions of the image and then determine the
+    # center
+    (h, w) = image.shape[:2]
+    (cX, cY) = (w // 2, h // 2)
+ 
+    # grab the rotation matrix (applying the negative of the
+    # angle to rotate clockwise), then grab the sine and cosine
+    # (i.e., the rotation components of the matrix)
+    M = cv2.getRotationMatrix2D((cX, cY), -angle, 1.0)
+    cos = np.abs(M[0, 0])
+    sin = np.abs(M[0, 1])
+ 
+    # compute the new bounding dimensions of the image
+    nW = int((h * sin) + (w * cos))
+    nH = int((h * cos) + (w * sin))
+ 
+    # adjust the rotation matrix to take into account translation
+    M[0, 2] += (nW / 2) - cX
+    M[1, 2] += (nH / 2) - cY
+ 
+    # perform the actual rotation and return the image
+    return cv2.warpAffine(image, M, (nW, nH))
+
 
 # Cropping
 # img = cv2.imread('../images/Lenna.png', 0)
@@ -50,18 +76,60 @@ from matplotlib import pyplot as plt
 # cv2.imwrite('../images/IVLenna.png', equl)
 
 # JPEG Compression
-img = cv2.imread('../images/Lenna.jpg', 0)
-# print(src.dtype)
-# img = np.zeros((512,512), dtype=src.dtype)
-# for i in range(512):
-#     for j in range(512):
-#         img[i][j] = 255 - src[i][j]
+# img = cv2.imread('../images/Lenna.jpg', 0)
+
+# blur = cv2.blur(img, (5, 5))
+# equl = cv2.equalizeHist(blur)
+
+
+# # cv2.imshow("img", src)
+# # cv2.waitKey(0)
+
+# cv2.imwrite('../images/JPCLenna.jpg', equl)
+
+# Rotate by degree
+# src = cv2.imread('../images/Lenna.png', 0)
+# # img = rotate_bound(src, 10)
+# # cv2.imshow("img", img)
+# # cv2.waitKey(0)
+
+# rotated05 = imutils.rotate(src, 5)
+# blur = cv2.blur(rotated05, (5, 5))
+# equl = cv2.equalizeHist(blur)
+# cv2.imwrite('../images/rotate05.png', equl)
+
+# rotated10 = imutils.rotate(src, 10)
+# blur = cv2.blur(rotated10, (5, 5))
+# equl = cv2.equalizeHist(blur)
+# cv2.imwrite('../images/rotate10.png', equl)
+
+# rotated20 = imutils.rotate(src, 20)
+# blur = cv2.blur(rotated20, (5, 5))
+# equl = cv2.equalizeHist(blur)
+# cv2.imwrite('../images/rotate20.png', equl)
+
+# rotated30 = imutils.rotate(src, 30)
+# blur = cv2.blur(rotated30, (5, 5))
+# equl = cv2.equalizeHist(blur)
+# cv2.imwrite('../images/rotate30.png', equl)
+
+# rotated40 = imutils.rotate(src, 40)
+# blur = cv2.blur(rotated40, (5, 5))
+# equl = cv2.equalizeHist(blur)
+# cv2.imwrite('../images/rotate40.png', equl)
+# # cv2.imshow("Rotated (Problematic)", rotated)
+# # cv2.waitKey(0)
+
+# % change of Pixel Values
+src = cv2.imread('../images/Lenna.png', 0)
+# print(src[10][10])
+img = np.zeros((512,512), dtype=src.dtype)
+# img[10][10] = 0.25 * src[10][10]
+# print(img[10][10])
+for i in range(512):
+    for j in range(512):
+        img[i][j] = 1 * src[i][j]
 
 blur = cv2.blur(img, (5, 5))
 equl = cv2.equalizeHist(blur)
-
-
-# cv2.imshow("img", src)
-# cv2.waitKey(0)
-
-cv2.imwrite('../images/JPCLenna.jpg', equl)
+cv2.imwrite('../images/Lenna100.png', equl)
