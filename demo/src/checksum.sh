9e78ded1f2d5139363161ba60db837027871e095
dahadaller <dahadaller@gmail.com>
2019-05-07 10:36:11 -0400

    bugs present in this, will fix @ skeith's

--- demo/src/checksum.sh
+++ demo/src/checksum.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 # David Hadaller
 
-current_hash="$(shasum -a 512 "$1" | cut -d" " -f1)"
+first_hash=$(shasum -a 512 "$1" | cut -d" " -f1)
 
 
 for argument in "$@"
@@ -9,21 +9,21 @@ for argument in "$@"
 do
 
 	printf "\nimage: ${argument} \nsha512 hash: $(shasum -a 512 $argument | cut -d" " -f1)"
-	previous_hash="${current_hash}"
-	current_hash="$(shasum -a 512 "$argument" | cut -d" " -f1)"
-	cum_xor="$(( 0x${current_hash} ^ 0x${previous_hash} ))"
-	if [ "${cum_xor}" -eq 0 ]
+	current_hash=$(shasum -a 512 "$argument" | cut -d" " -f1)
+	xor=$(( ${current_hash} ^ ${first_hash} ))
+	nxor=$(( ~ ${xor} ))
+	printf "\nbitwise xnor: ${nxor}"
+
+	if [ "${nxor}" -eq 0 ]
 	then
 		printf "\n"
 	else
-		python3 -c 'print ("\nPercent hash difference is {0:0.2f} %\n".format(abs((int("'${previous_hash}'", 16) - int("'${current_hash}'", 16))/int("'${previous_hash}'", 16) * 100)))'
+		#python3 -c 'print ("\nPercent hash difference is {0:0.2f} %\n".format(abs((int("'${previous_hash}'", 16) - int("'${current_hash}'", 16))/int("'${previous_hash}'", 16) * 100)))'
+		difference=$(echo ${xor} | tr -d 0 | wc -c)
+		abs_diff=${difference#-}
+		per_diff=$(( 100 * ${abs_diff} / 512 ))
+		printf "\nThe percentage difference is ${per_diff}%%\n"
 	fi
 done
 
-
-# if [ "${cum_xor}" -eq 0 ]
-# then
-# 	echo "All image hashes are the same."
-# else
-# 	echo "The image hashes are different."
-# fi
+printf " "
