7cf4c044a94161f6dd1c4eff56cd21d27409d4dc
Jonathan Tran <43768831+jontran1@users.noreply.github.com>
2019-09-12 15:57:30 -0400

    It turns out. You don't need Controller.java So i deleted the file. When you update your project, make sure to rebuild the project and it should work.

--- Production/src/com/nchash/Main.java
+++ Production/src/com/nchash/Main.java
@@ -1,11 +1,9 @@
 package com.nchash;
 
-import com.nchash.controller.Controller;
 import com.nchash.controller.MainController;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
-import javafx.scene.layout.BorderPane;
 import javafx.stage.Stage;
 
 public class Main extends Application {
