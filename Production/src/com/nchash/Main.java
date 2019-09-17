c7b6a7c2c8c84dd65592c9b22557998248c81f54
Jonathan Tran <43768831+jontran1@users.noreply.github.com>
2019-09-17 16:51:02 -0400

    removed folder
    removed folder, added test comment.

--- Production/src/com/nchash/Main.java
+++ Production/src/com/nchash/Main.java
@@ -5,7 +5,7 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
-
+//test comment
 public class Main extends Application {
 
     public static void main(String[] args) {
